e84e8fdd444cb03ad32089a4e1ded563b2014ada
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-11-27 10:28:45 +0100

    fetch data and display in result

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Search from "./components/Search";
-import Saved from "./components/Saved";
-import Result from "./components/Result";
 import { AppContainer } from "./App_style";
+import Darkmode from "./components/Darkmode";
+import Result from "./components/Result";
+import Saved from "./components/Saved";
+import Search from "./components/Search";
 
 function App() {
   return (
     <AppContainer>
       <h1>Dictionary</h1>
+      <Darkmode />
       <Search />
       <div>
         <Result />
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
 import Player from "./Player";
 
+interface Word {
+  word: string;
+  phonetic: string;
+  phonetics: Array<{ text: string; audio?: string }>;
+  origin: string;
+  meanings: Array<{
+    partOfSpeech: string;
+    definitions: Array<{
+      definition: string;
+      example?: string;
+      synonyms: string[];
+      antonyms: string[];
+    }>;
+  }>;
+}
+
 export default function Result() {
+  const [wordData, setwordData] = useState<Word[]>([]);
+  const fetchData = async () => {
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
+      .then((wordData) => {
+        if (!wordData.ok) {
+          throw new Error("wordData not ok");
+        }
+        return wordData.json();
+      })
+      .then((data) => {
+        setwordData(data);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch data:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <p>API result</p>
+      {wordData &&
+        wordData.map((word, index) => (
+          <div key={index}>
+            <h2>{word.word}</h2>
+            {word.meanings.map((defs, dindex) => (
+              <div key={dindex}>
+                {defs.definitions.map((def, i) => (
+                  <div key={i}>{def.definition}</div>
+                ))}
+              </div>
+            ))}
+            <div>{word.phonetic}</div>
+            <div>{word.origin}</div>
+          </div>
+        ))}
       <Player />
     </>
   );
