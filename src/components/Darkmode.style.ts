cc6636ca8f49fc724038d544d89a4e175b119f44
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-11-27 11:06:57 +0100

    implement darkmode

--- src/components/Darkmode.style.ts
+++ src/components/Darkmode.style.ts
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const DarkmodeBtn = styled.button`
   cursor: pointer;
+  width: 200px;
 `;
--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
 import { DarkmodeBtn } from "./Darkmode.style";
 
 export default function Darkmode() {
-  return <DarkmodeBtn>darkmode</DarkmodeBtn>;
+  const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+      setIsDark(true);
+      console.log("dark");
+    } else {
+      document.body.classList.remove("dark");
+      setIsDark(false);
+
+      console.log("remove dark");
+    }
+  }, [isDark]);
+
+  const toggleDarkmode = () => {
+    setIsDark(!isDark);
+  };
+
+  return <DarkmodeBtn onClick={toggleDarkmode}>darkmode</DarkmodeBtn>;
 }
