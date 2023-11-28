2e2ba5eadd3125aaa0ac5f7223de6e8412f8ef8e
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-11-28 16:38:19 +0100

    Separate word interface to its own file

---
+++ src/Interfaces.tsx
@@ -0,0 +1,15 @@
+export interface Word {
+  word: string;
+  phonetic: string;
+  phonetics: Array<{ text: string; audio?: string }>;
+  origin: string;
+  meanings: Array<{
+    partOfSpeech: string;
+    definitions: Array<{
+      definition: string;
+      example?: string;
+      synonyms: string[];
+      antonyms: string[];
+    }>;
+  }>;
+}
